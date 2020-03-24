f6a8820cb9f7853b2ae67d1da3f6b49ac5065d49
Antonio Vrbić <antonio.vrbic@gdi.net>
2020-03-24 13:53:33 +0100

    2020-03-24 13:24

--- index.js
+++ index.js
@@ -1,105 +1,128 @@
 const fs = require("fs").promises;
 const csv = require("csvtojson");
-const converter = require('json-2-csv');
+const converter = require("json-2-csv");
 
 (async () => {
-    let countiesGeoJson = JSON.parse(await fs.readFile('counties_original.geojson', 'UTF-8'));
-    let cases = await csv().fromFile("covid_hr.csv");
+  let countiesGeoJson = JSON.parse(
+    await fs.readFile("counties_original.geojson", "UTF-8")
+  );
+  let cases = await csv().fromFile("covid_hr.csv");
 
-    let casesPerCounty = sumarizeCases(cases, 'county');
-    let casesPerCity = sumarizeCases(cases, 'city');
+  let casesPerCounty = sumarizeCases(cases, "county");
+  let casesPerCity = sumarizeCases(cases, "city");
 
-    fs.writeFile('docs/casesPerCity.json', JSON.stringify(casesPerCity));
-    fs.writeFile('docs/casesPerCounty.json', JSON.stringify(casesPerCounty));
+  fs.writeFile("docs/casesPerCity.json", JSON.stringify(casesPerCity));
+  fs.writeFile("docs/casesPerCounty.json", JSON.stringify(casesPerCounty));
 
-    await generateCsv(casesPerCity, 'generated/cases_per_city.csv');
-    await generateCsv(casesPerCounty, 'generated/cases_per_county.csv');
+  await generateCsv(casesPerCity, "generated/cases_per_city.csv");
+  await generateCsv(casesPerCounty, "generated/cases_per_county.csv");
 
-    await generateCumulativeCasesCsv(cases);
+  await generateCumulativeCasesCsv(cases);
 
-    await generateCountyGeoJson(countiesGeoJson, casesPerCounty);
+  await generateCountyGeoJson(countiesGeoJson, casesPerCounty);
 })();
 
 async function generateCumulativeCasesCsv(casesRecords) {
-    let casesObject = {};
-    let cases = 0;
-    let recovered = 0;
-    let died = 0;
+  let casesObject = {};
+  let cases = 0;
+  let recovered = 0;
+  let died = 0;
 
-    for (const record of casesRecords) {
-        cases += +record.new_cases;
-        recovered += +record.new_recovered;
-        died += +record.new_died;
+  for (const record of casesRecords) {
+    cases += +record.new_cases;
+    recovered += +record.new_recovered;
+    died += +record.new_died;
 
-        if (!casesObject[record.date]) {
-            casesObject[record.date] = {
-                date: record.date,
-                cases: cases,
-                recovered: recovered,
-                died: died,
-                new_cases: +record.new_cases,
-                new_recovered: +record.new_recovered,
-                new_died: +record.new_died,
-            };
-        } else {
-            casesObject[record.date] = {
-                date: record.date,
-                cases: cases,
-                recovered: recovered,
-                died: died,
-                new_cases: casesObject[record.date].new_cases + +record.new_cases,
-                new_recovered: casesObject[record.date].new_recovered + +record.new_recovered,
-                new_died: casesObject[record.date].new_died + +record.new_died,
-            };
-        }
-
+    if (!casesObject[record.date]) {
+      casesObject[record.date] = {
+        date: record.date,
+        cases: cases,
+        recovered: recovered,
+        died: died,
+        new_cases: +record.new_cases,
+        new_recovered: +record.new_recovered,
+        new_died: +record.new_died
+      };
+    } else {
+      casesObject[record.date] = {
+        date: record.date,
+        cases: cases,
+        recovered: recovered,
+        died: died,
+        new_cases: casesObject[record.date].new_cases + +record.new_cases,
+        new_recovered:
+          casesObject[record.date].new_recovered + +record.new_recovered,
+        new_died: casesObject[record.date].new_died + +record.new_died
+      };
     }
+  }
 
-    console.log('cases:', cases);
-    console.log('recovered:', recovered);
-    console.log('died:', died);
+  console.log("cases:", cases);
+  console.log("recovered:", recovered);
+  console.log("died:", died);
 
-    fs.writeFile('docs/casesPerDay.json', JSON.stringify(Object.values(casesObject)));
+  fs.writeFile(
+    "docs/casesPerDay.json",
+    JSON.stringify(Object.values(casesObject))
+  );
 
-    await fs.writeFile('generated/cases_cumulative.csv', await converter.json2csvAsync(Object.values(casesObject)));
+  await fs.writeFile(
+    "generated/cases_cumulative.csv",
+    await converter.json2csvAsync(Object.values(casesObject))
+  );
 }
 
 async function generateCountyGeoJson(countiesGeoJson, casesPerCounty) {
-    for (const feature of countiesGeoJson.features) {
-        feature.properties.total_cases = casesPerCounty[feature.properties.name] ? casesPerCounty[feature.properties.name].cases : 0;
-        feature.properties.recovered = casesPerCounty[feature.properties.name] ? casesPerCounty[feature.properties.name].recovered : 0;
-        feature.properties.died = casesPerCounty[feature.properties.name] ? casesPerCounty[feature.properties.name].died : 0;
-        feature.properties.active = feature.properties.total_cases - feature.properties.recovered - feature.properties.died;
-    }
-    await fs.writeFile('generated/counties_hr_covid.geojson', JSON.stringify(countiesGeoJson));
-    await fs.writeFile('docs/counties_hr_covid.geojson', JSON.stringify(countiesGeoJson));
+  for (const feature of countiesGeoJson.features) {
+    feature.properties.total_cases = casesPerCounty[feature.properties.name]
+      ? casesPerCounty[feature.properties.name].cases
+      : 0;
+    feature.properties.recovered = casesPerCounty[feature.properties.name]
+      ? casesPerCounty[feature.properties.name].recovered
+      : 0;
+    feature.properties.died = casesPerCounty[feature.properties.name]
+      ? casesPerCounty[feature.properties.name].died
+      : 0;
+    feature.properties.active =
+      feature.properties.total_cases -
+      feature.properties.recovered -
+      feature.properties.died;
+  }
+  await fs.writeFile(
+    "generated/counties_hr_covid.geojson",
+    JSON.stringify(countiesGeoJson)
+  );
+  await fs.writeFile(
+    "docs/counties_hr_covid.geojson",
+    JSON.stringify(countiesGeoJson)
+  );
 }
 
 async function generateCsv(casesObject, path) {
-    let list = [];
-    for (const key in casesObject) {
-        if (casesObject.hasOwnProperty(key)) {
-            const element = casesObject[key];
-            list.push({ name: key, ...element });
-        }
+  let list = [];
+  for (const key in casesObject) {
+    if (casesObject.hasOwnProperty(key)) {
+      const element = casesObject[key];
+      list.push({ name: key, ...element });
     }
-    await fs.writeFile(path, await converter.json2csvAsync(list));
+  }
+  await fs.writeFile(path, await converter.json2csvAsync(list));
 }
 
 function sumarizeCases(cases, key) {
-    let casesPerKey = {};
-    for (const c of cases) {
-        if (!casesPerKey[c[key]]) {
-            casesPerKey[c[key]] = {
-                cases: 0,
-                recovered: 0,
-                died: 0
-            };
-        }
-        casesPerKey[c[key]].cases += +c.new_cases;
-        casesPerKey[c[key]].recovered += +c.new_recovered;
-        casesPerKey[c[key]].died += +c.new_died;
+  let casesPerKey = {};
+  for (const c of cases) {
+    if (!casesPerKey[c[key]]) {
+      casesPerKey[c[key]] = {
+        cases: 0,
+        recovered: 0,
+        died: 0
+      };
     }
-    console.table(casesPerKey);
-    return casesPerKey;
+    casesPerKey[c[key]].cases += +c.new_cases;
+    casesPerKey[c[key]].recovered += +c.new_recovered;
+    casesPerKey[c[key]].died += +c.new_died;
+  }
+  console.table(casesPerKey);
+  return casesPerKey;
 }
